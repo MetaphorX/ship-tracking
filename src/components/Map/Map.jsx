006d17881d398f3ada7718294f62a8fe7dba53b3
Shakur <gall.abdulshakur@gmail.com>
2022-10-27 22:11:39 +0100

    structure done

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useMemo} from 'react'
+import GoogleMapReact from 'google-map-react'
+import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api'
+import {Paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+
+import useStyles from './styles'
 
 const Map = () => {
-  return (
-    <div>Map</div>
+
+  const { isLoaded} = useLoadScript({
+    googleMapsApiKey: 'AIzaSyA93PU7-39O4Z2QU3DOjFNLR1AbkG2NmUg'
+  })
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px)')
+  
+  const coordinates ={ lat: 0, lng: 0}
+
+  
+
+  if(!isLoaded)return <div>Loading...</div>
+  return (<>
+    
+      <GoogleMap
+
+        center={{ lat: 0, lng: 0}}
+        zoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        
+        mapContainerClassName={classes.mapContainer}
+      >
+        <Marker position={{ lat: 0, lng: 0}}/>
+      </GoogleMap>
+
+    
+    
+    </>
   )
 }
 
